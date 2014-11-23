ca9d4e482fc287faad635a801bb9b5cb584cf13f
Takashi Maruyama <tksmaru@gmail.com>
2014-11-23 14:29:36 +0900

    'DBAccessorに外出し'

--- src/main/java/org/tksmaru/sandbox/jdbc/PreparedStatementSample.java
+++ src/main/java/org/tksmaru/sandbox/jdbc/PreparedStatementSample.java
@@ -1,8 +1,9 @@
 package org.tksmaru.sandbox.jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+
+import org.tksmaru.sandbox.jdbc.util.DBAccessor;
 
 public class PreparedStatementSample {
 
@@ -12,20 +13,15 @@ public class PreparedStatementSample {
 	 */
 	public static void main(String [] args) throws Exception {
 		
-		Class.forName("com.mysql.jdbc.Driver");
-
-		Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sandbox", "root", "root");
-		
-		System.out.println(conn);
+		Connection conn = DBAccessor.getConnection();
 		
 		// update 
 		PreparedStatement pstmt = conn.prepareStatement("update status set status = 2 where id = 1 and status = 1");
-		
 		int updated = pstmt.executeUpdate();
 		
 		System.out.println("updated column num = " + updated);
 
-		pstmt.close();
-		conn.close();
+		DBAccessor.closeStatement(pstmt);
+		DBAccessor.closeConnection(conn);
 	}
 }
